52bff515f28057a7a7a11e782d66a6fc5dbc7a3d
matt-hogan <23533809+matt-hogan@users.noreply.github.com>
2022-09-17 17:52:30 -0500

    Save charts as pngs

--- main.py
+++ main.py
@@ -16,14 +16,26 @@ data = yf.download(
     end="2022-09-17",
 )
 
+def make_dir(dir):
+    """ Create folder to store pngs """
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
 def get_closing_prices(ticker):
     """ Returns a list of the Adj Close prices for the provided ticker """
     return [ price for price in data["Adj Close"][ticker] ]
 
-for ticker in ticker_names:
-    ticker_prices = np.array(get_closing_prices(ticker))
-    plt.plot(ticker_prices)
-    plt.title(f"{ticker} Closing Stock Price For the Last 10 Days")
-    plt.xlabel("Day")
-    plt.ylabel("Price ($)")
-    plt.close()
+def create_charts(tickers):
+    """ Create chart for each stock and save as png """
+    charts_dir = "charts"
+    make_dir(charts_dir)
+    for ticker in tickers:
+        ticker_prices = np.array(get_closing_prices(ticker))
+        plt.plot(ticker_prices)
+        plt.title(f"{ticker} Closing Stock Price For the Last 10 Days")
+        plt.xlabel("Day")
+        plt.ylabel("Price ($)")
+        plt.savefig(os.path.join(charts_dir, f"{ticker}.png"))
+        plt.close()
+
+create_charts(ticker_names)
